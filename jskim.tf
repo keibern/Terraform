145f04f32d60fa159b37a72a0842e03a60b761b9
keibern <99776582+keibern@users.noreply.github.com>
2022-09-13 16:40:14 +0900

    Update jskim.tf

--- jskim.tf
+++ jskim.tf
@@ -1,7 +1,6 @@
 #Create VPC
 resource "aws_vpc" "vpc" {
     cidr_block           = "10.0.0.0/16"
-    instance_tenancy     = "default"
     enable_dns_hostnames = true      #default 값이 false
 
     tags = { 
